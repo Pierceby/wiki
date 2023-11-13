ce5d06faef35dcbccdcd01784b1ad0835753c54e
Pierceby <lyw4715046@163.com>
2023-11-13 19:33:53 +0800

    1.4--添加自定义配置文件测试自定义绑定类中的值

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world"+testHello;
     }
 }
