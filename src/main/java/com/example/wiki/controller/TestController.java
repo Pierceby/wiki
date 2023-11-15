d41c1dc9f9098461a86a2fc5ac28ff335b414138
Pierceby <lyw4715046@163.com>
2023-11-15 20:57:54 +0800

    1.7--集成mybatis-generator自动生成代码并增加demo表查询

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.wiki.controller;
 
+import com.example.wiki.domain.Demo;
 import com.example.wiki.domain.Test;
+import com.example.wiki.service.DemoService;
 import com.example.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,17 +13,10 @@ import java.util.List;
 
 @RestController
 public class TestController {
-    @Value("${test.hello:TEST}")
-    private String testHello;
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello world"+testHello;
-    }
-
     @Autowired
-    private TestService testService;
-    @GetMapping("/test/list")
-    public List<Test> list(){
-        return testService.list();
+    private DemoService demoService;
+    @GetMapping("/demo/list")
+    public List<Demo> list(){
+        return demoService.list();
     }
 }
