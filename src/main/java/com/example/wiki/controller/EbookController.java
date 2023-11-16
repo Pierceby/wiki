a791febfb7506391648fd7eab66273cf993bff9b
Pierceby <lyw4715046@163.com>
2023-11-16 21:25:33 +0800

    1.9--完成电子书列表查询接口

---
+++ src/main/java/com/example/wiki/controller/EbookController.java
@@ -0,0 +1,21 @@
+package com.example.wiki.controller;
+
+import com.example.wiki.domain.Demo;
+import com.example.wiki.domain.Ebook;
+import com.example.wiki.service.DemoService;
+import com.example.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EbookController {
+    @Autowired
+    private EbookService ebookService;
+    @GetMapping("/ebook/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
