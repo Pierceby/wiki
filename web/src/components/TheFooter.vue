703021f706223794e8ac042973bd0192f183a70f
Pierceby <lyw4715046@163.com>
2023-11-17 19:52:19 +0800

    2.5--制作TheFooter和TheHeader组件

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ebook
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+});
+</script>
+
+<script setup lang="ts">
+</script>
