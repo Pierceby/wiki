ead3e718659a4f42fe98e39f65dd5938f81e3536
Pierceby <lyw4715046@163.com>
2023-11-18 21:02:23 +0800

    3.9--增加aop，打印接口耗时、请求参数、返回参数；去掉过滤器和拦截器

--- src/main/java/com/example/wiki/config/springMvcConfig.java
+++ src/main/java/com/example/wiki/config/springMvcConfig.java
@@ -1,18 +1,18 @@
-package com.example.wiki.config;
-
-import com.example.wiki.interceptor.LogInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-public class springMvcConfig implements WebMvcConfigurer {
-    @Autowired
-    public LogInterceptor logInterceptor;
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-    }
-}
+//package com.example.wiki.config;
+//
+//import com.example.wiki.interceptor.LogInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//@Configuration
+//public class springMvcConfig implements WebMvcConfigurer {
+//    @Autowired
+//    public LogInterceptor logInterceptor;
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//}
