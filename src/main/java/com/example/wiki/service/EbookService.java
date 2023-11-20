9e697c4d47a3df0ba4ee0cbcae3c53b86ced95dc
Pierceby <lyw4715046@163.com>
2023-11-20 19:32:59 +0800

    6.4--集成pagehelper分页插件，降低mapper接口的日志级别为trace，方便查看打印日志

--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -8,6 +8,11 @@ import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookResp;
 import com.example.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.mysql.cj.log.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,14 +26,23 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
+    private static final Logger log= LoggerFactory.getLogger(EbookService.class);
+
     public List<EbookResp> list(EbookReq req){
+
         //模糊查询
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
+        //与想查询的操作放在一起，以免中间有其他查询导致分页失败
+        PageHelper.startPage(2,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        log.info("总行数：{}",pageInfo.getTotal());
+        log.info("总页数：{}",pageInfo.getPages());
+
 //        ArrayList<EbookResp> ebookReqList = new ArrayList<>();
 //        for(Ebook ebook:ebookList){
 //            EbookResp ebookResp = new EbookResp();
@@ -36,6 +50,7 @@ public class EbookService {
 //            ebookReqList.add(ebookResp);
 //        }
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
         return list;
     }
 }
