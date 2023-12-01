8d431e1253ec9f67174434fd443633e1f2ea8d02
Pierceby <lyw4715046@163.com>
2023-12-01 20:40:58 +0800

    7-6--电子书管理增加分类选择

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -31,6 +31,9 @@
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar"/>
         </template>
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
             <a-button type="primary" @click="showModal(record)">
@@ -64,11 +67,12 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name" />
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id" />
-      </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2Id" />
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children' }"
+            :options="level1"
+        />
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="textarea" />
@@ -108,14 +112,8 @@ export default defineComponent({
         dataIndex: 'name'
       },
       {
-        title: '分类一',
-        key: 'category1Id',
-        dataIndex: 'category1Id'
-      },
-      {
-        title: '分类二',
-        key: 'category2Id',
-        dataIndex: 'category2Id'
+        title: '分类',
+        slots: { customRender: 'category' }
       },
       {
         title: '文档数',
@@ -167,6 +165,7 @@ export default defineComponent({
       });
     };
     onMounted(() => {
+      handleQueryCategory();
       handleQuery({
         page:1,
         size:pagination.value.pageSize
@@ -176,15 +175,49 @@ export default defineComponent({
     const open = ref<boolean>(false);
     const confirmLoading = ref<boolean>(false);
     const modalText = ref<string>('Content of the modal');
-    const ebook=ref({});
+    const ebook=ref();
+    const level1 =  ref();
+    let categorys: any;
+    const categoryIds=ref();
+
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if(data.success){
+          categorys = data.content;
+          console.log("原始数组：",categorys);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1);
+        }else {
+          message.error(data.message);
+        }
+      });
+    };
+    const getCategoryName = (cid: number) => {
+      // console.log(cid)
+      let result = "";
+      categorys.forEach((item: any) => {
+        if (item.id === cid) {
+          // return item.name; // 注意，这里直接return不起作用
+          result = item.name;
+        }
+      });
+      return result;
+    };
 
     const showModal = (record:any) => {
       open.value = true;
       ebook.value=Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
 
     const handleOk = () => {
       confirmLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/ebook/save",ebook.value).then((response) => {
         confirmLoading.value = false;
         const data = response.data;
@@ -234,7 +267,10 @@ export default defineComponent({
       handleDelete,
       showModal,
       handleQuery,
-      ebook
+      ebook,
+      categoryIds,
+      level1,
+      getCategoryName
     }
   }
 });
