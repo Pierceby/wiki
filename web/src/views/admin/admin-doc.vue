c5ef45040a0d141374c62aead7ed48ba62d964a2
Pierceby <lyw4715046@163.com>
2023-12-21 19:59:36 +0800

    8-9--文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开就初始化富文本框不用延时，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -3,8 +3,8 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
-        <a-col :span="8">
+      <a-row :gutter="24">
+        <a-col :span="8" >
           <p>
             <a-form layout="inline" :model="param">
               <a-form-item>
@@ -25,10 +25,14 @@
               :data-source="level1"
               :pagination="false"
               :loading="loading"
+              size="small"
           >
+            <template #name="{ text, record }">
+              {{record.sort}} {{text}}
+            </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="showModal(record)">
+                <a-button type="primary" @click="showModal(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -37,7 +41,7 @@
                     cancel-text="否"
                     @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -47,12 +51,18 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span:6}" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name" />
-            </a-form-item>
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+              <a-input v-model:value="doc.name" placeholder="名称"/>
 
-            <a-form-item label="父文档">
               <a-tree-select
                   v-model:value="doc.parent"
                   show-search
@@ -66,11 +76,8 @@
                   :replaceFields="{label : 'name', key: 'id', value: 'id'}"
               >
               </a-tree-select>
-            </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort" />
-            </a-form-item>
-            <a-form-item label="内容">
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -84,7 +91,7 @@
 <!--      v-model:open="open"-->
 <!--      title="文档表单"-->
 <!--      :confirm-loading="confirmLoading"-->
-<!--      @ok="handleOk">-->
+<!--      @ok="handleSave">-->
 <!--    -->
 <!--  </a-modal>-->
 </template>
@@ -114,6 +121,7 @@ export default defineComponent({
     const docs = ref();
     const loading = ref(false);
     const editor=new E('#content');
+    editor.config.zIndex=0;
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -204,9 +212,6 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      setTimeout(function () {
-        editor.create();
-      },100)
     };
     const  showModal= (record: any) => {
       open.value = true;
@@ -218,25 +223,14 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-      setTimeout(function () {
-        editor.create();
-      },100)
 
     };
 
     const columns = [
       {
         title: '名称',
-        dataIndex: 'name'
-      },
-      {
-        title: '父文档',
-        key: 'parent',
-        dataIndex: 'parent'
-      },
-      {
-        title: '顺序',
-        dataIndex: 'sort'
+        dataIndex: 'name',
+        slots: { customRender: 'name' }
       },
       {
         title: 'Action',
@@ -265,6 +259,7 @@ export default defineComponent({
      */
     onMounted(() => {
       handleQuery();
+      editor.create();
     });
 
     const open = ref<boolean>(false);
@@ -272,7 +267,7 @@ export default defineComponent({
     const modalText = ref<string>('Content of the modal');
     const doc=ref({});
 
-    const handleOk = () => {
+    const handleSave = () => {
       confirmLoading.value = true;
       axios.post("/doc/save",doc.value).then((response) => {
         confirmLoading.value = false;
@@ -316,7 +311,7 @@ export default defineComponent({
       loading,
       confirmLoading,
       modalText,
-      handleOk,
+      handleSave,
       open,
       add,
       handleDelete,
